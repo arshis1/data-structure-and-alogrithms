b5f463299971bddbdfa304153dbd967008462698
Arshiya Shaikh <arshiyashaikh92@gmail.com>
2023-08-28 23:16:16 -0700

    Add files via upload

---
+++ recursion/perm_gen.py
@@ -0,0 +1,23 @@
+#take user input
+a = input("input the number of elements")
+print(a)
+
+for i in range(a):
+
+input_alphabets = ['a','b','c','d','e']
+answer = []
+
+for i in range(len(input_alphabets)):
+    for j in range(len((input_alphabets))):
+        if i!=j:
+            for k in range(len(input_alphabets)):
+                if k!=j and k!=i:
+                    for l in range(len(input_alphabets)):
+                        if l!=k and l!=j and l!=i:
+                            output = input_alphabets[i] + input_alphabets[j] + input_alphabets[k] + input_alphabets[l]
+                            if output in answer:
+                                continue
+                            else:
+                                answer.append(output)
+
+print(answer)
