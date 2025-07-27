014ff4197ff05b1dd8b5c8af18c333cc343a3b49
arshis1 <arshiyashaikh92@gmail.com>
2025-07-27 14:19:15 -0700

    Update binary_tree.py

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -1,4 +1,20 @@
 #####################################################
-#This program is the implementation of a binary tree
+#This program is the implementation of a binary tree: 2, 3,4, 5 
 #####################################################
 
+class Node:
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+# Initialize and allocate memory for tree nodes
+firstNode = Node(2)
+secondNode = Node(3)
+thirdNode = Node(4)
+fourthNode = Node(5)
+
+# Connect binary tree nodes
+firstNode.left = secondNode
+firstNode.right = thirdNode
+secondNode.left = fourthNode
