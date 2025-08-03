d3399d80c752ab5cb2bbcc89966ccfb96c2aee70
arshis1 <arshiyashaikh92@gmail.com>
2025-08-03 13:37:33 -0700

    Binary Tree

--- 1.data_structures/2.binary_tree/binary_tree.py
+++ 1.data_structures/2.binary_tree/binary_tree.py
@@ -1,20 +1,40 @@
-#####################################################
-#This program is the implementation of a binary tree: 2, 3,4, 5 
-#####################################################
+##################################################################
+#This program is the implementation of a binary tree: 2, 3, 4, 5 
+#################################################################
 
 class Node:
     def __init__(self, key):
         self.val = key
         self.left = None
         self.right = None
+        self.count = 1 #to handle duplicates.
 
-# Initialize and allocate memory for tree nodes
-firstNode = Node(2)
-secondNode = Node(3)
-thirdNode = Node(4)
-fourthNode = Node(5)
+    def insert(self, value):
+        if value == self.val:
+            self.count +=1
+        elif value < self.val:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.count +=1
+                self.left.insert(value)
+        elif value > self.val:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
 
-# Connect binary tree nodes
-firstNode.left = secondNode
-firstNode.right = thirdNode
-secondNode.left = fourthNode
+    def search(self, target):
+        if target == self.value:
+            return True 
+        elif target <self.value:
+            #since this is binary tree, we can ignore the right subtree.
+            if self.left is None:
+                return  False
+            else:
+                return self.left.search(target)
+        else: #target > self.value
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(target)
