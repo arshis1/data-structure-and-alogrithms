0682b5ed339c1df7d48249d50c97241117e7402d
arshis1 <arshiyashaikh92@gmail.com>
2025-08-05 19:00:27 -0700

    Create linkedin_list.py

---
+++ 1.data_structures/3.linked_list_and_doubly_linked_list/linkedin_list.py
@@ -0,0 +1,32 @@
+#######################################
+# Linked List Implementation in Python #
+#######################################
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+    def insert(value):
+        current.next = Node(value)
+        current = current.next
+
+    def delete(value):
+    
+    def search():
+
+    
+
+
+if __name__ == "__main__":
+    
+    head = None
+    n  =int(input("Enter number of nodes: "))
+
+    for i in range(n):
+        value = int(input("Enter node value : "))
+        if head is None:
+            head = Node(value)
+        else:
+            head.insert(value)
+
